19bf386cd4f30e29435a633a3fbc71a517666069
TheSanvi <sanvivermavns@gmail.com>
2024-01-08 18:33:47 +0530

    code added

---
+++ questions/simple_interest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int P;
+    int R;
+    int T;
+    cin>>P;cin>>R;cin>>T;
+
+    int SimpleInterest = P*R*T/100;
+    cout<<SimpleInterest;
+}
