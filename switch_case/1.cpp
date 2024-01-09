8536431252cfb5de70cdd8466cae845411a7ef74
TheSanvi <sanvivermavns@gmail.com>
2024-01-09 18:17:30 +0530

    code added

---
+++ switch_case/1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int val;
+    cin>>val;
+    cout<<"Enter The Value"<<endl;
+
+    switch(val){
+        case 1: cout<<"Sanvi";
+        break;
+        case 2: cout<<"hellu";
+        break;
+        case 3: cout<<"dance";
+        break;
+        default:cout<<"chal hat";
+    }
+    
+}
